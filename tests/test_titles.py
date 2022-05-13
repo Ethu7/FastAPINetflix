b32867edcde6f0362b3dc2176eeb7212af1860a2
Ethu7 <ethan1675@gmail.com>
2022-05-13 12:27:09 -0400

    CICD

--- tests/test_titles.py
+++ tests/test_titles.py
@@ -24,118 +24,15 @@ def test_unauthorized_user_get_one_post(client, test_titles):
     assert res.status_code == 401
 
 
-def test_get_one_post_not_exist(authorized_client, test_titles):
+def test_get_one_title_not_exist(authorized_client, test_titles):
     res = authorized_client.get(f"/posts/88888")
     assert res.status_code == 404
 
 
-def test_get_one_post(authorized_client, test_titles):
+def test_get_one_title(authorized_client, test_titles):
     res = authorized_client.get(f"/posts/{test_titles[0].id}")
     title = schemas.PostOut(**res.json())
     assert title.Title.show_id == test_titles[0].show_id
     assert title.Post.title == test_titles[0].title
 
 
-@pytest.mark.parametrize("title, show_id, published", [
-    ("awesome new title", "awesome new content", True),
-    ("favorite pizza", "i love pepperoni", False),
-    ("tallest skyscrapers", "wahoo", True),
-])
-def test_create_post(authorized_client, test_user, test_posts, title, content, published):
-    res = authorized_client.post(
-        "/posts/", json={"title": title, "content": content, "published": published})
-
-    created_post = schemas.Post(**res.json())
-    assert res.status_code == 201
-    assert created_post.title == title
-    assert created_post.content == content
-    assert created_post.published == published
-    assert created_post.owner_id == test_user['id']
-
-
-def test_create_post_default_published_true(authorized_client, test_user, test_posts):
-    res = authorized_client.post(
-        "/posts/", json={"title": "arbitrary title", "content": "aasdfjasdf"})
-
-    created_post = schemas.Post(**res.json())
-    assert res.status_code == 201
-    assert created_post.title == "arbitrary title"
-    assert created_post.content == "aasdfjasdf"
-    assert created_post.published == True
-    assert created_post.owner_id == test_user['id']
-
-
-def test_unauthorized_user_create_post(client, test_user, test_posts):
-    res = client.post(
-        "/posts/", json={"title": "arbitrary title", "content": "aasdfjasdf"})
-    assert res.status_code == 401
-
-
-def test_unauthorized_user_delete_Post(client, test_user, test_posts):
-    res = client.delete(
-        f"/posts/{test_posts[0].id}")
-    assert res.status_code == 401
-
-
-def test_delete_post_success(authorized_client, test_user, test_posts):
-    res = authorized_client.delete(
-        f"/posts/{test_posts[0].id}")
-
-    assert res.status_code == 204
-
-
-def test_delete_post_non_exist(authorized_client, test_user, test_posts):
-    res = authorized_client.delete(
-        f"/posts/8000000")
-
-    assert res.status_code == 404
-
-
-def test_delete_other_user_post(authorized_client, test_user, test_posts):
-    res = authorized_client.delete(
-        f"/posts/{test_posts[3].id}")
-    assert res.status_code == 403
-
-
-def test_update_post(authorized_client, test_user, test_posts):
-    data = {
-        "title": "updated title",
-        "content": "updatd content",
-        "id": test_posts[0].id
-
-    }
-    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
-    updated_post = schemas.Post(**res.json())
-    assert res.status_code == 200
-    assert updated_post.title == data['title']
-    assert updated_post.content == data['content']
-
-
-def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
-    data = {
-        "title": "updated title",
-        "content": "updatd content",
-        "id": test_posts[3].id
-
-    }
-    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
-    assert res.status_code == 403
-
-
-def test_unauthorized_user_update_post(client, test_user, test_posts):
-    res = client.put(
-        f"/posts/{test_posts[0].id}")
-    assert res.status_code == 401
-
-
-def test_update_post_non_exist(authorized_client, test_user, test_posts):
-    data = {
-        "title": "updated title",
-        "content": "updatd content",
-        "id": test_posts[3].id
-
-    }
-    res = authorized_client.put(
-        f"/posts/8000000", json=data)
-
-    assert res.status_code == 404
