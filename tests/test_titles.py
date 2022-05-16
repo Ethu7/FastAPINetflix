4c9d7171a127ad3bccf9732fc672250a309e46c5
Ethu7 <ethan1675@gmail.com>
2022-05-16 18:50:44 -0400

    edit test

--- tests/test_titles.py
+++ tests/test_titles.py
@@ -2,11 +2,12 @@ import pytest
 from app import schemas
 
 
+
 def test_get_all_titles(authorized_client, test_titles):
-    res = authorized_client.get("/posts/")
+    res = authorized_client.get("/titles/")
 
     def validate(title):
-        return schemas.PostOut(**title)
+        return schemas.TitleOut(**title)
     titles_map = map(validate, res.json())
     titles_list = list(titles_map)
 
@@ -14,25 +15,25 @@ def test_get_all_titles(authorized_client, test_titles):
     assert res.status_code == 200
 
 
-def test_unauthorized_user_get_all_posts(client, test_titles):
+def test_unauthorized_user_get_all_titless(client, test_titles):
     res = client.get("/titles/")
     assert res.status_code == 401
 
 
-def test_unauthorized_user_get_one_post(client, test_titles):
-    res = client.get(f"/posts/{test_titles[0].id}")
+def test_unauthorized_user_get_one_title(client, test_titles):
+    res = client.get(f"/titles/{test_titles[0].id}")
     assert res.status_code == 401
 
 
 def test_get_one_title_not_exist(authorized_client, test_titles):
-    res = authorized_client.get(f"/posts/88888")
+    res = authorized_client.get(f"/titles/7881234")
     assert res.status_code == 404
 
 
 def test_get_one_title(authorized_client, test_titles):
-    res = authorized_client.get(f"/posts/{test_titles[0].id}")
-    title = schemas.PostOut(**res.json())
+    res = authorized_client.get(f"/titles/{test_titles[0].id}")
+    title = schemas.TitleOut(**res.json())
     assert title.Title.show_id == test_titles[0].show_id
-    assert title.Post.title == test_titles[0].title
+    assert title.TItle.title == test_titles[0].title
 
 
